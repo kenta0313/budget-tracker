e6a5044c92782fb696b11dc210ddd43aad55c6a7
kenta0313 <asanokenta@me.com>
2021-07-30 12:03:57 +0900

    新しい経費を追加できるようにする

--- src/components/ExpenseTotal.tsx
+++ src/components/ExpenseTotal.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const ExpenseTotal = () => {
+	const { expenses } = useContext(AppContext);
+
+	const totalExpenses = expenses.reduce((total, item) => {
+		return (total += item.cost);
+	}, 0);
+
 	return (
 		<div className='alert alert-primary'>
-			<span>使用額: 1000円</span>
+			<span>使用額: {totalExpenses}円</span>
 		</div>
 	);
 };
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -5,8 +5,13 @@ interface AppProviderProps {
 
 }
 
-const AppReducer = (state: any, action: { type: any; }) => {
+const AppReducer = (state: any, action: any) => {
 	switch (action.type) {
+		case 'ADD_EXPENSE':
+			return {
+				...state,
+				expenses: [...state.expenses, action.payload],
+			};
 		default:
 			return state;
 	}
